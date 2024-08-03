334ddd3be36c3b11dcc337e4a003eeb8c47b1fee
paschal533 <okwuosahpaschal@gmail.com>
2024-08-03 19:55:07 +0100

    contract update

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,6 +1,36 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require('./scripts/mine');
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_API_URL = process.env.API_URL;
+const SEPOLIA_PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  paths: {
+    sources: "./src/backend/contracts",
+    artifacts: "./src/backend/artifacts",
+    cache: "./src/backend/cache",
+    tests: "./src/backend/test"
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: process.env.SEED_PHRASE,
+      },
+      chainId: 1337,
+    },
+    sepolia: {
+      url: SEPOLIA_API_URL,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
