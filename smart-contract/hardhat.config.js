a1cc5fda6a0a2c48501adf5155f4c4ee7a8af51d
paschal533 <okwuosahpaschal@gmail.com>
2024-08-06 21:47:02 +0100

    feat:update hardhat

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -21,7 +22,24 @@ module.exports = {
    optimismSepolia : {
       url: 'https://sepolia.optimism.io',
       accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 1000000000,
     },
+  'base-sepolia': {
+      url: 'https://sepolia.base.org',
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
+  alfajores : {
+      url: 'https://alfajores-forno.celo-testnet.org',
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
+   mode: {
+      url: "https://sepolia.mode.network",
+      chainId: 919,
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 1000000000,
+    }
  },
  etherscan: {
     apiKey: {
@@ -36,7 +54,23 @@ module.exports = {
           apiURL: "https://optimism-sepolia.blockscout.com/api",
           browserURL: "https://optimism-sepolia.blockscout.com/",
         }
-      }
+      },
+      {
+        network: "base-sepolia",
+        chainId: 84532,
+        urls: {
+         apiURL: "https://base-sepolia.blockscout.com/api",
+         browserURL: "https://base-sepolia.blockscout.com"
+        }
+      },
+      {
+        network: "mode",
+        chainId: 919,
+        urls: {
+         apiURL: "https://sepolia.explorer.mode.network/api",
+         browserURL: "https://sepolia.explorer.mode.network"
+        }
+      },
     ]
   },
   sourcify: {
